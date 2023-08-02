b4211d44ebc5e8aa103d703e862faab7f500c111
Prasadmts <prasadmts2507@gmail.com>
2023-08-02 22:57:41 +0530

    submitting code-Wed Aug 02 2023 22:57:35 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,33 @@
-import { useState } from "react";
+
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
 
-  const [text, setText] = useState("");
+  function greetings(e) {
+    e.preventDefault();
 
-  function handleChange(e) {
-    setText(e.target.value);
+    let greet = e.target.value;
+
+    if(greet != "") {
+      document.getElementById("greetings").innerHTML = `Hello ${greet}!`;
+    }
+    else {
+      document.getElementById("greetings").innerHTML = "";
+    }
   }
-  
+
   return (
     <div>
-      <p>Enter your name:</p>
-      <input value={text} onChange={handleChange} />
-      <p>Hello {text}!</p>
+        {/* Do not remove the main div */}
+        <form>
+          <label for="name">Enter your name:</label>
+          <br />
+          <input id="name" type="text" onInput={greetings} />
+          <p id="greetings"></p>
+        </form>
     </div>
-  );
+  )
 }
 
 export default App
