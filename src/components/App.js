8e54d18c317861001647fc5fbafe7c29a72bd866
Prasadmts <prasadmts2507@gmail.com>
2023-08-02 22:44:20 +0530

    submitting code-Wed Aug 02 2023 22:44:03 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
+import { useState } from "react";
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [text, setText] = useState("");
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Enter your name:</p>
+      <input value={text} onChange={handleChange} />
+      <p>Hello {text}!</p>
     </div>
-  )
+  );
 }
 
 export default App
